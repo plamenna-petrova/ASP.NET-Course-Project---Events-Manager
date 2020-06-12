886bc6250af363a641749a8761aff9de1b6ad64e
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2020-06-12 15:46:55 +0300

    Importing project

---
+++ Events-Lab/Events.Web/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿namespace Events.Web.Controllers
+{
+    using Events.Data;
+    using Microsoft.AspNet.Identity;
+    using System.Web.Mvc;
+
+    [ValidateInput(false)]
+
+    public class BaseController : Controller
+    {
+        protected ApplicationDbContext db = new ApplicationDbContext();
+
+        public bool IsAdmin()
+        {
+            var currentUserId = this.User.Identity.GetUserId();
+            var isAdmin = (currentUserId != null && this.User.IsInRole("Administrator"));
+            return isAdmin;
+        }
+    }
+}
---
+++ Events-Lab/Events.Web/Models/CommentViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Events.Web.Models
+{
+    using Events.Data;
+    using System;
+    using System.Linq.Expressions;
+
+    public class CommentViewModel
+    {
+        public string Text { get; set; }
+
+        public string Author { get; set; }
+
+        public static Expression<Func<Comment, CommentViewModel>> ViewModel
+        {
+            get
+            {
+                return c => new CommentViewModel()
+                {
+                    Text = c.Text,
+                    Author = c.Author.FullName
+                };
+            }
+        }
+    }
+}
